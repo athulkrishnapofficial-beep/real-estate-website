0de0942e7af1568efcc7a946d8c8ce476a24f477
athulkrishnap25 <athulkrishnapjio@gmail.com>
2025-10-27 13:34:03 +0530

    Add homepage, components, and Firebase setup

--- epic-properties/src/components/PropertyFilter.jsx
+++ epic-properties/src/components/PropertyFilter.jsx
@@ -63,7 +63,7 @@ const PropertyFilter = ({ onFilterChange }) => {
               className="w-full p-2 border border-gray-600 rounded-md text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
             >
               <option value="">Select Type</option>
-              <option value="Apartment">Apartment</option>
+              <option value="Apartments">Apartments</option>
               <option value="Boutique Houses">Boutique Houses</option>
               <option value="House">House</option>
               <option value="Land">Land</option>
